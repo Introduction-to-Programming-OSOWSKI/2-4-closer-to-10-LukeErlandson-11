0ca3365329d268a12f86e2b1ebe2753f655a6763
LukeErlandson-11 <826619@mystma.org>
2022-09-21 14:35:02 -0500

    3

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def close10(x, y):
     if abs(10-x)>abs(10-y):
-        return x
-    elif abs(10-x)<abs(10-y):
         return y
+    elif abs(10-x)<abs(10-y):
+        return x
     else:
         return 0
 
-print(close10(15, 5))
+
